2fb327353a165ab85c7b904c05c78fb25958fd51
Mallorax <patryk733773@gmail.com>
2020-11-02 20:46:25 +0100

    lekcja 3 zad 2

---
+++ lesson3/lesson_3/Assets/ex2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ex2 : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private bool dirRight;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        dirRight = true;
+        rb.isKinematic = true;
+    }
+
+    private void FixedUpdate()
+    {
+        // vector1 = vector1.normalized * speed * Time.deltaTime;
+        //vector2 = vector2.normalized * speed * Time.deltaTime;
+        if (dirRight)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(-Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (transform.position.x > 10)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.x < 0)
+        {
+            dirRight = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
